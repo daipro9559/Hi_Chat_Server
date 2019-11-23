de2a99fad92b684d16930562dc97a5b96fa9ed48
dainv <daipro9559@gmail.com>
2019-11-23 17:46:47 +0700

    Auto stash before cherrypick of "add git ignore"

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'
-var IceServer = require ('./model/iceServer')
+var IceServer = require('./model/iceServer')
 const SOCKET_CONSTANTS = require('./util/constants')
 var express = require("express");
 var app = express();
@@ -26,8 +26,7 @@ app.get('/', (req, res) => {
 var userConnectToRoom; //  value test
 var iceServer1 = new IceServer('stun:[2404:6800:4008:c02::7f]:19302', ["stun:[2404:6800:4008:c02::7f]:19302"], "", "", "")
 var iceServer2 = new IceServer("stun:[2404:6800:4008:c06::7f]:19302", ["stun:[2404:6800:4008:c06::7f]:19302"], "", "", "")
-var iceServer3 = new IceServer("turn:[2404:6800:4008:c02::7f]:19305?transport=tcp",
-    ["turn:[2404:6800:4008:c02::7f]:19305?transport=tcp"],
+var iceServer3 = new IceServer("turn:[2404:6800:4008:c02::7f]:19305?transport=tcp", ["turn:[2404:6800:4008:c02::7f]:19305?transport=tcp"],
     "CN/b3+4FEgbSO1cY5DIYzc/s6OMTIICjBQ", "OclB8wswXlVn5xBHVIUyjDq97/U=", "")
 const iceServers = [iceServer1, iceServer2, iceServer3]
 
@@ -46,34 +45,34 @@ io.on("connection", (socket) => {
             let currentCount = roomSocket.length
             if (currentCount < 2) {
                 socket.join(room)
-                callback({ isInit: false, isFull: false , iceServers: iceServers, dataRooms:dataRooms[room]})
+                callback({ isInit: false, isFull: false, iceServers: iceServers, dataRooms: dataRooms[room] })
                 return
             }
-            callback({ isInit: false, isFull: true})
+            callback({ isInit: false, isFull: true })
         } else {
             socket.join(room)
-            callback({ isInit: true, isFull: false, iceServers: iceServers})
-            // create data save in room
+            callback({ isInit: true, isFull: false, iceServers: iceServers })
+                // create data save in room
             dataRooms[room] = {}
-            dataRooms[room].iceCandidates=[]
+            dataRooms[room].iceCandidates = []
         }
 
     });
-    socket.on(SOCKET_CONSTANTS.SDP_EVENT, (room,data)=>{
+    socket.on(SOCKET_CONSTANTS.SDP_EVENT, (room, data) => {
         console.log('data: ' + data);
         let sdp = JSON.parse(data)
-        if(sdp.type === "OFFER"){
+        if (sdp.type === "OFFER") {
             dataRooms[room].sdp = data
-        }else{
+        } else {
             socket.to(room).emit(SOCKET_CONSTANTS.SDP_EVENT, data)
         }
     })
-    socket.on(SOCKET_CONSTANTS.ICE_CANDIDATE_EVENT, (room, data) =>{
+    socket.on(SOCKET_CONSTANTS.ICE_CANDIDATE_EVENT, (room, data) => {
         console.log('data: ' + data);
         let roomSocket = io.sockets.adapter.rooms[room];
-        if(roomSocket.length == 1){
+        if (roomSocket.length == 1) {
             dataRooms[room].iceCandidates.push(data)
-        }else{
+        } else {
             socket.to(room).emit(SOCKET_CONSTANTS.ICE_CANDIDATE_EVENT, data)
         }
     })
--- model/iceServer.js
+++ model/iceServer.js
@@ -1,11 +1,12 @@
 class IceServer {
-    tlsCertPolicy = 'TLS_CERT_POLICY_SECURE'
     constructor(uri, urls, userName, password, hostname) {
         this.uri = uri
         this.urls = urls
         this.username = userName
         this.password = password
         this.hostname = hostname
+        this.tlsCertPolicy = "TLS_CERT_POLICY_SECURE"
+
     }
 }
 
